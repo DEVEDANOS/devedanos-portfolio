8a64dd15703773c47ce72938ebed9cb0ccb0a007
daproclaima <sn.itpro@gmail.com>
2021-06-08 22:03:55 +0200

    add configuration for tailwind, postcss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,38 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/templates/**/*.hbs',
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  },
+};
 
 module.exports = function (defaults) {
   // eslint-disable-next-line prefer-const
   let app = new EmberApp(defaults, {
     fingerprint: {
       extensions: ['js', 'css', 'map'],
+    },
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules'],
+            },
+          },
+          require('tailwindcss')('./app/tailwind/config.js'),
+          ...isProduction ? [purgeCSS] : [],
+        ],
+      },
     },
   });
 
