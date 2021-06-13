70818319e6c7bd0d20ffabf2132c9b2c96900de1
daproclaima <sn.itpro@gmail.com>
2021-06-13 13:41:52 +0200

    change navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,8 +1,18 @@
 import Component from '@glimmer/component';
 
 export default class NavbarComponent extends Component {
-  toggleLinks () {
-    const listLinks = document.querySelector('.nav-dropdown');
-    listLinks.classList.toggle('hidden');
+  changeMobileMenuVisibility () {
+    const mobileMenu = document.querySelector('#mobile-menu');
+    const buttonMobileMenuClosed = document.querySelector('.nav-main__button-mobile-menu--closed');
+    const buttonMobileMenuOpened = document.querySelector('.nav-main__button-mobile-menu--opened');
+
+    mobileMenu.classList.toggle('hidden');
+
+    if (mobileMenu.classList.contains('hidden')) {
+      buttonMobileMenuClosed.classList.toggle('hidden');
+      return buttonMobileMenuOpened.classList.toggle('hidden');
+    }
+    buttonMobileMenuClosed.classList.toggle('hidden');
+    return buttonMobileMenuOpened.classList.toggle('hidden');
   }
 }
