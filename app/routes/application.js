39264435cf10ed5c9264b51edcce8d1a7f561e62
daproclaima <sn.itpro@gmail.com>
2021-06-14 00:28:11 +0200

    refactor application.js to use beforeModel hook
    change button cta style

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,9 +4,7 @@ import { inject as service } from '@ember/service';
 export default class ApplicationRoute extends Route {
   @service intl;
 
-  constructor () {
-    super(...arguments);
-    const primaryLocale = this.intl.get('primaryLocale');
-    this.intl.setLocale([primaryLocale]);
+  beforeModel () {
+    this.intl.setLocale(['fr-fr']);
   }
 }
