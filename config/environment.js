f90b93db046dfb9cb13402d9ee345f853160c9b2
daproclaima <sn.itpro@gmail.com>
2021-06-15 21:12:25 +0200

    refactor banner-usp
    refactor banner-container-container and banner-content
    
    clean up banners.scss

--- config/environment.js
+++ config/environment.js
@@ -48,7 +48,7 @@ module.exports = function (environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     // http://ember-service-worker.com/documentation/configuration/#disabling-the-service-worker
-    ENV.APP.SW_DISABLED = true;
+    // ENV.APP.SW_DISABLED = true;
   }
 
   if (environment === 'test') {
