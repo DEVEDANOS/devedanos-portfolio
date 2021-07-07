9840c255088ca3d3df3dd71ffd057c2b2677f3f7
daproclaima <sn.itpro@gmail.com>
2021-07-07 09:57:26 +0200

    refactor through js lint in whole project

--- tests/unit/routes/legal-notices-test.js
+++ tests/unit/routes/legal-notices-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | legal-notices', function(hooks) {
+module('Unit | Route | legal-notices', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    let route = this.owner.lookup('route:legal-notices');
+  test('it exists', function (assert) {
+    const route = this.owner.lookup('route:legal-notices');
     assert.ok(route);
   });
 });
