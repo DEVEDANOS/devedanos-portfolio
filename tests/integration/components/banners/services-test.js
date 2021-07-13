cea45efadc173d5c097cba3db5b42c92a6997b13
daproclaima <sn.itpro@gmail.com>
2021-07-13 16:32:30 +0200

    add banner for services
    refactor classes and style for banners cta and creations
    
    add translations for services banner and creations
    
    add images for services
    
    update translations for services

---
+++ tests/integration/components/banners/services-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | banners/services', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Banners::Services />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      <Banners::Services>
+        template block text
+      </Banners::Services>
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
