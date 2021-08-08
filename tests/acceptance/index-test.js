3837ce90ead342aa02f4d323815c721017c9f5ef
daproclaima <sn.itpro@gmail.com>
2021-08-09 00:42:54 +0200

    add about-me.hbs data-test on resume link to fix acceptance/index-test.js

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,32 +1,30 @@
 import { module, test } from 'qunit';
 import { visit, click, /* pauseTest */ } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { setupIntl, setLocale, t } from 'ember-intl/test-support';
+import { setupIntl, t } from 'ember-intl/test-support';
 
 module('Acceptance | index', function (hooks) {
   setupApplicationTest(hooks);
   setupIntl(hooks);
 
   test('it changes language to en-us', async function (assert) {
-    setLocale('en-us');
 
     await visit('/');
     await click('[data-test=language-picker-button]');
     await click('[data-test=language-picker-en-us]');
 
     assert
-      .dom('[data-test-app=link-resume]')
+      .dom('[data-test=link-resume]')
       .hasText(t('pages.home.banners.about-me.line-4'));
   });
 
   test('it changes language to fr-fr', async function (assert) {
-    setLocale('fr-fr');
     await visit('/');
     await click('[data-test=language-picker-button]');
     await click('[data-test=language-picker-fr-fr]');
 
     assert
-      .dom('[data-test-app=link-resume]')
+      .dom('[data-test=link-resume]')
       .hasText(t('pages.home.banners.about-me.line-4'));
   });
 });
