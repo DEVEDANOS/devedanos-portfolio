c874198c689e5eb01fe3ed29af6aa91fba52ba13
daproclaima <sn.itpro@gmail.com>
2021-08-11 23:44:25 +0200

    update pdf resumes

--- app/controllers/resume.js
+++ app/controllers/resume.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import returnLinkIntl from 'devedanos-portfolio/utils/return-link-intl';
 
-const resumeLinkEn = 'freelance_full-stack_developer';
-const resumeLinkFr = 'fr_freelance_full-stack_developer';
+const resumeLinkEn = 'freelance_fullstack_developer';
+const resumeLinkFr = 'fr_freelance_fullstack_developer';
 const mapResumeUrls = new Map();
 mapResumeUrls.set('en-us', resumeLinkEn);
 mapResumeUrls.set('fr-fr', resumeLinkFr);
