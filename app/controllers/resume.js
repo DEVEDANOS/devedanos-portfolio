a50153efd07d2e10eaf0f56a480f9811c308c3f0
daproclaima <sebastien@devedanos,com>
2022-05-07 13:40:48 +0200

    feat: update resumes

--- app/controllers/resume.js
+++ app/controllers/resume.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import returnLinkIntl from 'devedanos-portfolio/utils/return-link-intl';
 
-const resumeLinkEn = 'freelance_fullstack_developer';
-const resumeLinkFr = 'fr_freelance_fullstack_developer';
+const resumeLinkEn = 'en_detailed_resume';
+const resumeLinkFr = 'fr_detailed_resume';
 const mapResumeUrls = new Map();
 mapResumeUrls.set('en-us', resumeLinkEn);
 mapResumeUrls.set('fr-fr', resumeLinkFr);
