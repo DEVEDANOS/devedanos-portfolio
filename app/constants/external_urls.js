9bc05c78035d88d0e471f129bb41f21d26eae2ca
daproclaima <sebastien@devedanos,com>
2023-11-22 11:28:36 +0100

    feat: update cta links

--- app/constants/external_urls.js
+++ app/constants/external_urls.js
@@ -1,3 +1,3 @@
-export const URL_FORM_CTA_EN = 'https://calendly.com/devedanos/software-idea-call';
-export const URL_FORM_CTA_FR = 'https://calendly.com/devedanos/software-idea-call';
+export const URL_FORM_CTA_EN = 'https://calendly.com/snobour/software-idea';
+export const URL_FORM_CTA_FR = 'https://calendly.com/snobour/software-idea';
 export const CONTACT_EMAIL = 'sebastien@devedanos.com';
